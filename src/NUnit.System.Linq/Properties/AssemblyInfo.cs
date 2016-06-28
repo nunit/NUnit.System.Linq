8ec60df839f310663ef6f696602d79b0836b4950
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-06-28 09:13:29 +0200

    Added GitVersionTask 3.5.4

--- src/NUnit.System.Linq/Properties/AssemblyInfo.cs
+++ src/NUnit.System.Linq/Properties/AssemblyInfo.cs
@@ -22,17 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7ed9849-5dde-4e23-8390-76a80acde2f8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-
 [assembly: CLSCompliant(true)]
